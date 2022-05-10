b3a466f57564a8ca7b9a9b47e598d01509c9e832
haeyoung <withbb96@gmail.com>
2022-05-11 00:38:52 +0900

    정적컨텐츠, mvc

--- src/main/java/seazero/springdotcom/controller/HelloController.java
+++ src/main/java/seazero/springdotcom/controller/HelloController.java
@@ -3,6 +3,7 @@ package seazero.springdotcom.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,5 +12,11 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "spring!!");
         return "hello"; //resources > templates > hello.html (hello로 가서 랜더링 하라는거)
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
